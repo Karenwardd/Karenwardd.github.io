e2e27614511bed7a0183c35290335e130f3e708e
Karenwardd <92968637+Karenwardd@users.noreply.github.com>
2022-02-21 00:57:07 -0500

    Add files via upload

---
+++ Assign 4.5/scripts.js
@@ -0,0 +1,18 @@
+
+
+//window.onload = function()
+{
+//console.log (document.getElementsByTagName("li"));
+};
+
+{
+//console.log (document.getElementsByTagName("div"));
+	
+	$("button").click(function(){
+  $("button").hide(1000);
+});
+
+	
+	$("content").hide(){
+	$("content").show();}
+	
